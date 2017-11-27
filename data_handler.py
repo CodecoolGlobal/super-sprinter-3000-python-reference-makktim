a974bae6f0b15e40e2fda504b0859314c6d9b75f
Daniel Salamon <daniel.salamon@codecool.com>
2017-11-27 13:11:48 +0100

    Added USer Story table with static data

---
+++ data_handler.py
@@ -0,0 +1,46 @@
+def get_all_user_story():
+    user_stories = [
+        {
+            "id": 1,
+            "title": "List all User Stories",
+            "story": """
+                As a User,
+                I want to see all the previously saved User stories,
+                So I get an overview of all requirements
+            """,
+            "criteria": """
+                Given that there are any number ({0..n}) of saved User Stories,
+                When I open the website (`/`),
+                Then ensure I see a table with all the stored data,
+                And if there is no stored data,
+                Then hide the table
+            """,
+            "value": 1000,
+            "estimation": 2,
+            "status": "new"
+        },
+        {
+            "id": 2,
+            "title": "Add new User Stories",
+            "story": """
+                As a User,
+                I want to be able to add a new User stories,
+                So I can extend the list of requirements
+            """,
+            "criteria": """
+                When I open the website (`/`),
+                And I click the "Add User Story" button
+                Then ensure I see form where I can add details of the new User Story,
+                And if I submit the form, this US gets saved.
+            """,
+            "value": 800,
+            "estimation": 4,
+            "status": "new"
+        }
+    ]
+
+    for user_story in user_stories:
+        user_story['story'] = user_story['story'].replace('\n', '<br>')
+        user_story['criteria'] = user_story['criteria'].replace('\n', '<br>')
+
+    return user_stories
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 
+import data_handler
+
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/list')
 def route_list():
-    return render_template('list.html')
+    user_stories = data_handler.get_all_user_story()
+
+    return render_template('list.html', user_stories=user_stories)
 
 
 if __name__ == '__main__':
