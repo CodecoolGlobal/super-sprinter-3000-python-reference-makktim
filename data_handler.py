ea3fbfa3c950df751adab170b724868969c62595
Daniel Salamon <daniel.salamon@codecool.com>
2017-11-27 20:11:03 +0100

    Fix csv headers

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,7 @@
 import csv
 
 DATA_FILE_PATH = 'data.csv'
-DATA_HEADER = ['id', 'title', 'story', 'criteria', 'value', 'estimation', 'status']
+DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 
 
 def get_all_user_story():
@@ -9,18 +9,18 @@ def get_all_user_story():
     user_stories = []
 
     #  open csv file to read
-    with open(DATA_FILE_PATH) as csvfile:
+    with open(DATA_FILE_PATH, encoding='utf-8') as csvfile:
         #  use DictReader to directly create dictionaries from each lines in the csv file
         reader = csv.DictReader(csvfile)
 
         #  read all lines in csv file
         for row in reader:
             #  make a copy of the read row, since we can't modify it
-            user_story = row
+            user_story = dict(row)
 
             #  allow multiline strings to display in HTML
-            user_story['story'] = user_story['story'].replace('\n', '<br>')
-            user_story['criteria'] = user_story['criteria'].replace('\n', '<br>')
+            user_story['user_story'] = user_story['user_story']
+            user_story['acceptance_criteria'] = user_story['acceptance_criteria']
 
             #  store modified data in temporary list
             user_stories.append(user_story)
