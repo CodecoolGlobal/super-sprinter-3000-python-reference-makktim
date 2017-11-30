a37a58d3477bb4831031de524875afda98ed672f
Daniel Salamon <daniel.salamon@codecool.com>
2017-11-30 20:33:35 +0100

    Added more tests for Story adding page

--- test/tests/add.js
+++ test/tests/add.js
@@ -24,40 +24,70 @@ describe('Add User Story', () => {
     }));
 
     it('should show a header', test(async () => {
-        const h1Text = await page.evaluate(() => document.querySelector('h1').innerText);
-
-        expect(h1Text).to.be.equal('Add User Story');
+        expect(
+            await page.$eval('h1', el => el.innerText)
+        ).to.be.equal('Add User Story');
     }));
 
     it('should contain a form', test(async () => {
-        const form = await page.evaluate(() => document.querySelector('form'));
-
-        expect(form).not.to.be.null;
+        expect(
+            await page.$('form')
+        ).not.to.be.null;
     }));
 
     it('should submit the form with post method', test(async () => {
-        const method = await page.evaluate(() => document.querySelector('form').getAttribute('method'));
-
-        expect(method.toLowerCase()).to.be.equal('post');
+        expect(
+            await page.$eval('form', el => el.getAttribute('method').toLowerCase())
+        ).to.contain('post');
     }));
 
     it('should submit the form to the same/similar route', test(async () => {
-        const action = await page.evaluate(() => document.querySelector('form').getAttribute('action'));
-
-        expect(action.toLowerCase()).to.contain('story');
+        expect(
+            await page.$eval('form', el => el.getAttribute('action').toLowerCase())
+        ).to.contain('story');
     }));
 
     it('should have a label for the story title', test(async () => {
-        const labelElement = await page.evaluate(() => document.querySelector('label[for="title"]'));
-
-        expect(labelElement).not.to.be.null;
+        expect(
+            await page.$('label[for="title"]')
+        ).not.to.be.null;
     }));
 
     it('should have a label for the story title', test(async () => {
-        const labelText = await page.evaluate(() => document.querySelector('label[for="title"]').innerHTML);
-
-        expect(labelText.toLowerCase()).to.contain('story title');
+        expect(
+            await page.$eval('label[for="title"]', el => el.innerHTML.toLowerCase())
+        ).to.contain('story title');
     }));
 
-    //body > form > p:nth-child(2) > label
+    it('should have a text input field for the story title with proper name attribute', test(async () => {
+        expect(
+            await page.$('input[name="title"]')
+        ).not.to.be.null;
+    }));
+
+    it('should have a text input field for the story title with proper id attribute', test(async () => {
+        expect(
+            await page.$('input#title')
+        ).not.to.be.null;
+    }));
+
+    it('should have a text input field for the story title with proper type attribute', test(async () => {
+        expect(
+            await page.$eval('#title', el => el.getAttribute('type'))
+        ).to.be.equal('text');
+    }));
+
+    it('should have a text input field for the story title with length validation', test(async () => {
+        expect(
+            await page.$eval('#title', el => el.getAttribute('minlength'))
+        ).to.be.equal('5');
+    }));
+
+    it('should have a text input field for the story title that is editable', test(async () => {
+        await page.type('#title', 'Test User Story');
+
+        expect(
+            await page.$eval('#title', el => el.value)
+        ).to.be.equal('Test User Story');
+    }));
 });
