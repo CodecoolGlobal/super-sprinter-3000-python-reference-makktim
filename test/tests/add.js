95b864e41ed924d85a231b4095d8b8e3846f2f44
Daniel Salamon <daniel.salamon@codecool.com>
2017-11-30 21:03:09 +0100

    Reformatted test structures to use multiple levels

--- test/tests/add.js
+++ test/tests/add.js
@@ -1,93 +1,103 @@
 const {expect} = require('chai');
 const {test} = require('../browser');
 
+let page, response;
+
+/**
+ * Load the page and store it for future tests.
+ */
+before(test(async (browser, opts) => {
+    page = await browser.newPage();
+    response = await page.goto(`${opts.appUrl}/story`);
+}));
+
 describe('Add User Story', () => {
-    let page, response;
+    describe('Page', () => {
+        it('should load without Exception', test(async () => {
+            // Check for Flask debug messages
+            expect(await page.$('.traceback')).to.be.null;
+            expect(await page.$('.debugger')).to.be.null;
+        }));
 
-    /**
-     * Load the page and store it for future tests.
-     */
-    before(test(async (browser, opts) => {
-        page = await browser.newPage();
-        response = await page.goto(`${opts.appUrl}/story`);
-    }));
+        it('should load without Error outside debug mode', test(async () => {
+            expect(await response.ok).to.be.true;
+            expect(await response.text()).not.to.contain('Server Error');
+        }));
 
-    it('loads the page without Exception', test(async () => {
-        // Check for Flask debug messages
-        expect(await page.$('.traceback')).to.be.null;
-        expect(await page.$('.debugger')).to.be.null;
-    }));
+        it('should show a header', test(async () => {
+            expect(
+                await page.$eval('h1', el => el.innerText)
+            ).to.be.equal('Add User Story');
+        }));
 
-    it('loads the page without Error outside debug mode', test(async () => {
-        expect(await response.ok).to.be.true;
-        expect(await response.text()).not.to.contain('Server Error');
-    }));
+        it('should contain a form', test(async () => {
+            expect(
+                await page.$('form')
+            ).not.to.be.null;
+        }));
 
-    it('should show a header', test(async () => {
-        expect(
-            await page.$eval('h1', el => el.innerText)
-        ).to.be.equal('Add User Story');
-    }));
+        it('should submit the form with post method', test(async () => {
+            expect(
+                await page.$eval('form', el => el.getAttribute('method').toLowerCase())
+            ).to.contain('post');
+        }));
 
-    it('should contain a form', test(async () => {
-        expect(
-            await page.$('form')
-        ).not.to.be.null;
-    }));
+        it('should submit the form to the same/similar route', test(async () => {
+            expect(
+                await page.$eval('form', el => el.getAttribute('action').toLowerCase())
+            ).to.contain('story');
+        }));
 
-    it('should submit the form with post method', test(async () => {
-        expect(
-            await page.$eval('form', el => el.getAttribute('method').toLowerCase())
-        ).to.contain('post');
-    }));
+    });
 
-    it('should submit the form to the same/similar route', test(async () => {
-        expect(
-            await page.$eval('form', el => el.getAttribute('action').toLowerCase())
-        ).to.contain('story');
-    }));
+    describe('Story title input', () => {
 
-    it('should have a label for the story title', test(async () => {
-        expect(
-            await page.$('label[for="title"]')
-        ).not.to.be.null;
-    }));
+        describe('Label', () => {
+            it('should be visible', test(async () => {
+                expect(
+                    await page.$('label[for="title"]')
+                ).not.to.be.null;
+            }));
 
-    it('should have a label for the story title', test(async () => {
-        expect(
-            await page.$eval('label[for="title"]', el => el.innerHTML.toLowerCase())
-        ).to.contain('story title');
-    }));
+            it('should have a proper text', test(async () => {
+                expect(
+                    await page.$eval('label[for="title"]', el => el.innerHTML.toLowerCase())
+                ).to.contain('story').and.contain('title');
+            }));
+        });
 
-    it('should have a text input field for the story title with proper name attribute', test(async () => {
-        expect(
-            await page.$('input[name="title"]')
-        ).not.to.be.null;
-    }));
+        describe('Input', () => {
+            it('should have a proper name attribute', test(async () => {
+                expect(
+                    await page.$('input[name="title"]')
+                ).not.to.be.null;
+            }));
 
-    it('should have a text input field for the story title with proper id attribute', test(async () => {
-        expect(
-            await page.$('input#title')
-        ).not.to.be.null;
-    }));
+            it('should have a proper id attribute', test(async () => {
+                expect(
+                    await page.$('input#title')
+                ).not.to.be.null;
+            }));
 
-    it('should have a text input field for the story title with proper type attribute', test(async () => {
-        expect(
-            await page.$eval('#title', el => el.getAttribute('type'))
-        ).to.be.equal('text');
-    }));
+            it('should have a proper type attribute', test(async () => {
+                expect(
+                    await page.$eval('#title', el => el.getAttribute('type'))
+                ).to.be.equal('text');
+            }));
 
-    it('should have a text input field for the story title with length validation', test(async () => {
-        expect(
-            await page.$eval('#title', el => el.getAttribute('minlength'))
-        ).to.be.equal('5');
-    }));
+            it('should have a length validation', test(async () => {
+                expect(
+                    await page.$eval('#title', el => el.getAttribute('minlength'))
+                ).to.be.equal('5');
+            }));
 
-    it('should have a text input field for the story title that is editable', test(async () => {
-        await page.type('#title', 'Test User Story');
+            it('should be editable', test(async () => {
+                await page.type('#title', 'Test User Story');
 
-        expect(
-            await page.$eval('#title', el => el.value)
-        ).to.be.equal('Test User Story');
-    }));
+                expect(
+                    await page.$eval('#title', el => el.value)
+                ).to.be.equal('Test User Story');
+            }));
+        });
+    });
 });
--- test/tests/list.js
+++ test/tests/list.js
@@ -1,91 +1,94 @@
 const {expect} = require('chai');
 const {test} = require('../browser');
 
+let page, response;
+
+/**
+ * Load the page and store it for future tests.
+ */
+before(test(async (browser, opts) => {
+    page = await browser.newPage();
+    response = await page.goto(`${opts.appUrl}/`);
+}));
+
 describe('List all User Stories', () => {
-    let page, response;
+    describe('Page', () => {
+        it('should load without Exception', test(async () => {
+            // Check for Flask debug messages
+            expect(await page.$('.traceback')).to.be.null;
+            expect(await page.$('.debugger')).to.be.null;
+        }));
 
-    /**
-     * Load the page and store it for future tests.
-     */
-    before(test(async (browser, opts) => {
-        page = await browser.newPage();
-        response = await page.goto(`${opts.appUrl}/`);
-    }));
+        it('should load without Error outside debug mode', test(async () => {
+            expect(await response.ok).to.be.true;
+            expect(await response.text()).not.to.contain('Server Error');
+        }));
 
-    it('loads the page without Exception', test(async () => {
-        // Check for Flask debug messages
-        expect(await page.$('.traceback')).to.be.null;
-        expect(await page.$('.debugger')).to.be.null;
-    }));
+        it('should show application title as a header', test(async () => {
+            expect(
+                await page.$eval('h1', el => el.innerText)
+            ).to.be.equal('Super Sprinter 3000');
+        }));
+    });
+    describe('Table', () => {
+        it('should be visible', test(async () => {
+            expect(
+                await page.$('table')
+            ).not.to.be.null;
+        }));
 
-    it('loads the page without Error outside debug mode', test(async () => {
-        expect(await response.ok).to.be.true;
-        expect(await response.text()).not.to.contain('Server Error');
-    }));
+        it('should contain 4 rows', test(async () => {
+            expect(
+                await page.$eval('table tr', el => el.length)
+            ).not.to.be.equal(4);
+        }));
 
-    it('should show application title as a header', test(async () => {
-        expect(
-            await page.$eval('h1', el => el.innerText)
-        ).to.be.equal('Super Sprinter 3000');
-    }));
+        it('should contain the 7 column headers', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th', el => el.length)
+            ).not.to.be.equal(7);
+        }));
 
-    it('should contain a table', test(async () => {
-        expect(
-            await page.$('table')
-        ).not.to.be.null;
-    }));
+        it('should contain the id column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(1)', el => el.innerText.toLowerCase())
+            ).to.contain('id');
+        }));
 
-    it('table should contain 4 rows', test(async () => {
-        expect(
-            await page.$eval('table tr', el => el.length)
-        ).not.to.be.equal(4);
-    }));
+        it('should contain the title column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(2)', el => el.innerText.toLowerCase())
+            ).to.contain('title');
+        }));
 
-    it('table should contain the 7 column headers', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th', el => el.length)
-        ).not.to.be.equal(7);
-    }));
+        it('should contain the user_story column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(3)', el => el.innerText.toLowerCase())
+            ).to.contain('user').and.contain('story');
+        }));
 
-    it('table should contain the id column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(1)', el => el.innerText.toLowerCase())
-        ).to.contain('id');
-    }));
+        it('should contain the acceptance_criteria column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(4)', el => el.innerText.toLowerCase())
+            ).to.contain('acceptance').and.contain('criteria');
+        }));
 
-    it('table should contain the title column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(2)', el => el.innerText.toLowerCase())
-        ).to.contain('title');
-    }));
+        it('should contain the business_value column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(5)', el => el.innerText.toLowerCase())
+            ).to.contain('business').and.contain('value');
+        }));
 
-    it('table should contain the user_story column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(3)', el => el.innerText.toLowerCase())
-        ).to.contain('user').and.contain('story');
-    }));
+        it('should contain the estimation column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(6)', el => el.innerText.toLowerCase())
+            ).to.contain('estimation');
+        }));
 
-    it('table should contain the acceptance_criteria column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(4)', el => el.innerText.toLowerCase())
-        ).to.contain('acceptance').and.contain('criteria');
-    }));
-
-    it('table should contain the business_value column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(5)', el => el.innerText.toLowerCase())
-        ).to.contain('business').and.contain('value');
-    }));
-
-    it('table should contain the estimation column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(6)', el => el.innerText.toLowerCase())
-        ).to.contain('estimation');
-    }));
-
-    it('table should contain the status column header', test(async () => {
-        expect(
-            await page.$eval('table tr:nth-of-type(1) th:nth-of-type(7)', el => el.innerText.toLowerCase())
-        ).to.contain('status');
-    }));
+        it('should contain the status column header', test(async () => {
+            expect(
+                await page.$eval('table tr:nth-of-type(1) th:nth-of-type(7)', el => el.innerText.toLowerCase())
+            ).to.contain('status');
+        }));
+    });
 });
